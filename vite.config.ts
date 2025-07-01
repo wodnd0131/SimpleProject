39a84d55307fa7bd46d72f0b1e22ece9241dfa07
황재웅 <wodnd0131@naver.com>
2025-07-01 16:58:10 +0900

    cicd:git page 배포

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/SimpleProject/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
+    viteStaticCopy({
+      targets: [{ src: "dist/index.html", dest: ".", rename: "404.html" }],
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
